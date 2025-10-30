6d3b36aee15f62426a1aea3405c4a8fcd554e546
Ahmad Adptr <ahmadadptr@gmail.com>
2025-10-30 17:15:00 +0800

    menambahkan fungsi baru untuk mendapatkan single product berdasarkan id product

--- services/products.js
+++ services/products.js
@@ -1,11 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getAllProducts = async () => {
   const url = 'https://dummyjson.com/products';
   try {
     const response = await axios.get(url);
-    return response.data
+    return response.data;
   } catch (err) {
-    return err.message
+    return err.message;
   }
-}
+};
+
+export const getSingleProducts = async (id) => {
+  const url = 'https://dummyjson.com/products' + id;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+};
