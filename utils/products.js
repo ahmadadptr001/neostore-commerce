8be978c06c070ba965b84d3c93f2dddb621d75fb
Ahmad Adptr <ahmadadptr@gmail.com>
2025-10-30 16:50:53 +0800

    nenambahkan fungsi getAllCateogryName untuk memfilter kategori produk

--- utils/products.js
+++ utils/products.js
@@ -2,7 +2,7 @@ export const bestSelling = (allProducts) => {
   return allProducts.products.sort((a, b) => b.rating - a.rating).slice(0, 6);
 };
 
-const getAllCategoryName = (allProducts) => {
+export const getAllCategoryName = (allProducts) => {
   const allCategory = [];
   allProducts.map(item => {
     allCategory.push(item.category)
