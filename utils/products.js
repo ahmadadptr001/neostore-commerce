f0216dce746e59a637c6d493f9d2376af3616f05
Ahmad Adptr <ahmadadptr@gmail.com>
2025-10-30 20:34:17 +0800

    menambahkan fungsi mengubah string ISO ke objek Date.

--- utils/products.js
+++ utils/products.js
@@ -16,3 +16,9 @@ export const getPriceDiscount = (discount, price) => {
   const priceFixed = priceNow.toFixed(2);
   return priceFixed;
 }
+
+export function formatDate(isoDateString) {
+  const date = new Date(isoDateString);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
