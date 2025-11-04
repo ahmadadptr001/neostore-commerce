3680f526399789c8184febbe90f9e9dcab89f082
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-04 11:04:19 +0800

    ui: penambahan model baru

---
+++ UI/Models/BagModel.jsx
@@ -0,0 +1,21 @@
+'use client';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Model() {
+  const { scene } = useGLTF('/bag.glb'); // simpan model.glb di /public/models
+  return <primitive object={scene} scale={1.5} />;
+}
+
+export default function BagModel() {
+  return (
+    <div className="w-auto h-auto">
+      <Canvas camera={{ position: [0, 1, -2], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[2, 2, 2]} intensity={1} />
+        <Model />
+        <OrbitControls enableZoom autoRotate autoRotateSpeed={2} />
+      </Canvas>
+    </div>
+  );
+}
