77091c46ea366c52156da51499fd55a039c1e527
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-04 11:05:14 +0800

    style: penghapus styling yang tidak diperlukan

--- UI/Models/LaptopModel.jsx
+++ UI/Models/LaptopModel.jsx
@@ -1,15 +1,15 @@
-'use client'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+'use client';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 
 function Model() {
-  const { scene } = useGLTF('/laptop.glb') // simpan model.glb di /public/models
-  return <primitive object={scene} scale={1.5} />
+  const { scene } = useGLTF('/laptop.glb'); // simpan model.glb di /public/models
+  return <primitive object={scene} scale={1.5} />;
 }
 
 export default function LaptopModel() {
   return (
-    <div className="w-full h-[400px] md:h-[500px] bg-gray-900 rounded-xl overflow-hidden">
+    <div className="w-auto h-auto">
       <Canvas camera={{ position: [0, 1, 3], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} intensity={1} />
@@ -17,5 +17,5 @@ export default function LaptopModel() {
         <OrbitControls enableZoom autoRotate autoRotateSpeed={2} />
       </Canvas>
     </div>
-  )
+  );
 }
