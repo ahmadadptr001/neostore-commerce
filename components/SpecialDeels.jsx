a889acfe86a534c518d81ee6e0933b7032c2b0bb
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-04 10:33:15 +0800

    style: perubahan font-weight dan warna background

--- components/SpecialDeels.jsx
+++ components/SpecialDeels.jsx
@@ -32,10 +32,10 @@ export default function SpecialDeels() {
         <h2 className='text-3xl font-semibold'>Special Deals Available Now</h2>
         <p className='text-gray-300'>Get up to 80% off on all categories</p>
         {timeLeft.length !== 0 ? (
-          <div className="flex items-center gap-7  justify-center py-3">
+          <div className="flex items-center gap-6  justify-center py-3">
             {timeLeft.map((time, i) => (
-              <div key={i} className="flex flex-col">
-                <h3 className="font-bold text-4xl">{time}</h3>
+              <div key={i} className="flex flex-col bg-white/20 p-3">
+                <h3 className="font-extrabold text-4xl">{time}</h3>
                 <p className="text-gray-300 text-xs">{tagTime[i]}</p>
               </div>
             ))}
@@ -43,7 +43,7 @@ export default function SpecialDeels() {
         ) : (
           <p className='text-red-400 font-semibold'>This offer has expired!</p>
         )}
-        <Link href="/products" className="btn bg-white w-fit mx-auto mt-5">
+        <Link href="/products" className="btn bg-white w-fit mx-auto mt-3">
           <p className='text-[#9333EA]'>Shop Sale Now</p>
         </Link>
       </div>
