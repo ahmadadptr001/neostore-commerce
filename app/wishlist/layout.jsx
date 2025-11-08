33ef4ce88452d8f3c2ffea42f4bd0e130d0a68d3
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-08 19:32:56 +0800

    page: add page wishlist

---
+++ app/wishlist/layout.jsx
@@ -0,0 +1,17 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
+export const metadata = {
+  title: 'NeoStor products - Your Next-Gen Shopping Experience',
+  description: 'Pilih produk yang sesuai dengan Anda',
+};
+
+export default function RootLayoutWishList({ children }) {
+  return (
+      <div className="bg-gradient-to-b from-base-200 via-base-100 to-base-300 min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+  );
+}
