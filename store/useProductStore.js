df2827fb83591031a23c364ce94739dec4403500
Ahmad Adptr <ahmadadptr@gmail.com>
2025-11-08 15:29:11 +0800

    store: tambah fungsi addToCart & updateQuantity

--- store/useProductStore.js
+++ store/useProductStore.js
@@ -1,10 +1,37 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useProductStore = create((set) => ({
-  produk: [],
-  ambilProduk: async () => {
-    const res = await fetch("https://fakestoreapi.com/products");
-    const data = await res.json();
-    set({ produk: data });
-  },
-}));
+// code by ahmadadptr
+export const useProductStore = create(
+  persist(
+    (set, get) => ({
+      cart: [],
+
+      addToCart: (product) => {
+        const existing = get().cart.find((item) => item.id === product.id);
+        if (existing) {
+          set({
+            cart: get().cart.map((item) => {
+              item.id === product.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item;
+            }),
+          });
+        } else {
+          set({
+            cart: [...get().cart, product],
+          });
+        }
+      },
+
+      updateQuantity: (productID, quantity) => {
+        set({
+          cart : get().cart.map(item => item.id === productID ? {...item, quantity} : item)
+        })
+      }
+    }),
+    {
+      name: 'cart-storage',
+    }
+  )
+);
